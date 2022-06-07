35dddb7bbb56de2455a35a3b17078e6ba4ffd76a
sngyn <seye125@naver.com>
2022-06-07 21:33:10 +0900

    updates

---
+++ app/src/main/java/com/example/termproject/MemoExercise.java
@@ -0,0 +1,14 @@
+package com.example.termproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memo_exercise);
+    }
+}
