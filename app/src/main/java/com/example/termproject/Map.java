fdf1bda8344e48e412dcf81a0da73cf030bb8643
yuuuky02 <kim08146@naver.com>
2022-06-07 10:36:41 +0900

    memo_daily

---
+++ app/src/main/java/com/example/termproject/Map.java
@@ -0,0 +1,27 @@
+package com.example.termproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Map extends AppCompatActivity {
+
+    Button btn11;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        btn11 = findViewById(R.id.button11);
+
+        // 뒤로가기
+        btn11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/termproject/Stats.java
@@ -0,0 +1,59 @@
+package com.example.termproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+public class Stats extends AppCompatActivity {
+
+    NumberPicker yearPicker, monthPicker;
+    private static final int MAX_YEAR = 2030;
+    private static final int MIN_YEAR = 2010;
+
+    Button btn;
+    TextView tv;
+    View datedialog;
+
+    String date;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+        btn = findViewById(R.id.button);
+        tv = findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datedialog = View.inflate(Stats.this, R.layout.datedialog, null);
+                yearPicker = datedialog.findViewById(R.id.yearPicker);
+                monthPicker = datedialog.findViewById(R.id.monthPicker);
+                monthPicker.setMinValue(1);
+                monthPicker.setMaxValue(12);
+                yearPicker.setMinValue(MIN_YEAR);
+                yearPicker.setMaxValue(MAX_YEAR);
+
+                new AlertDialog.Builder(Stats.this)
+                        .setTitle("날짜 선택")
+                        .setIcon(R.drawable.calendar_month)
+                        .setView(datedialog)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                date = String.valueOf(yearPicker.getValue()) +". "
+                                        + String.valueOf(monthPicker.getValue());
+                                tv.setText(date);
+                            }
+                        })
+                        .setNegativeButton("취소", null)
+                        .show();
+            }
+        });
+    }
+}
